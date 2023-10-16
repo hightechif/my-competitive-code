e30f526e840860c115a67227864a0ab11f1eef7c
R. Fadhil <ridhan.fadhilah@sg-dsa.com>
2023-10-16 17:52:24 +0700

    TOKI - P71 - Menggambar Pegunungan

---
+++ toki/belajar/dasar/P71GambarGunung.py
@@ -0,0 +1,16 @@
+# P71GambarGunung.py : program utk menggambar pola gunung dari input integer.
+# Ridhan Fadhilah, 16-10-2023
+
+num = int(input())
+
+def count(i, x):
+    memo = [] # memoization
+    for i in range(1,x+1): # change recursive with simple looping with memoization
+      print("*" * i)
+      memo.append(i)
+      for j in range(0, len(memo)-1):
+        print("*" * memo[j])
+        memo.append(memo[j])
+
+if (__name__ == "__main__"):
+    count(1, num)
